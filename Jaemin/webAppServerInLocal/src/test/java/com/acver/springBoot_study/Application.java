7b981ddc7f2c6d994f19ac625e8fb1f807392976
Acver <devacver@gmail.com>
2022-01-13 16:31:26 +0900

    [new] 2주차

---
+++ Jaemin/webAppServerInLocal/src/test/java/com/acver/springBoot_study/Application.java
@@ -0,0 +1,11 @@
+package com.acver.springBoot_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String args[]){
+        SpringApplication.run(com.acver.springBoot_study.Application.class, args);
+    }
+}
