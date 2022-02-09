99ae3b928e2a3445480c8455483793f20bbdad79
Yoonoh <xkakak142@naver.com>
2022-02-09 15:55:33 +0900

    [Add] Add Role to user for post

--- Yoonoh/src/main/java/org/example/yoonoh/springboot/config/auth/dto/OAuthAttributes.java
+++ Yoonoh/src/main/java/org/example/yoonoh/springboot/config/auth/dto/OAuthAttributes.java
@@ -58,7 +58,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
